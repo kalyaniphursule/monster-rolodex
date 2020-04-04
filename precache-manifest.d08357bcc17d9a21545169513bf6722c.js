8dcee9077a397525cae3e4159a86894dd90c4f2d
kalyaniphursule <kalyaniphursule@gmail.com>
2020-04-04 07:26:55 +0530

    Updates

---
+++ precache-manifest.d08357bcc17d9a21545169513bf6722c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "629e1c198d29f9b5da0d593b67c5a0c7",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "1366c8265d3630a43ca7",
+    "url": "/monster-rolodex/static/css/main.82202253.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1366c8265d3630a43ca7",
+    "url": "/monster-rolodex/static/js/main.c7ed7fa1.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
